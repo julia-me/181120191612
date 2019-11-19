f44e2774a6557379fe8330b4ad3f5f1665eab292
Julia <onatskaya.julia@gmail.com>
2019-11-19 19:45:35 +0200

    task is done

---
+++ main.js
@@ -0,0 +1,53 @@
+const sendCommentBtn = document.getElementsByClassName("comment__button")[0];
+const commentValue = document.querySelector(".comment__add");
+const container = document.querySelector(".feedbacks__content");
+
+function runOnKeys(func) {
+  var codes = [].slice.call(arguments, 1);
+  var pressed = {};
+  document.onkeydown = function(e) {
+    e = e || window.event;
+    pressed[e.keyCode] = true;
+    for (var i = 0; i < codes.length; i++) {
+      if (!pressed[codes[i]]) {
+        return;
+      }
+    }
+    pressed = {};
+    func();
+  };
+
+  document.onkeyup = function(e) {
+    e = e || window.event;
+    delete pressed[e.keyCode];
+  };
+}
+
+const sendCommentHendler = () => {
+  if (commentValue.value.length) {
+    let newDate = new Date().toLocaleString("ru", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+    let correctDate = newDate
+      .split("")
+      .splice(0, newDate.length - 2)
+      .join("");
+    container.innerHTML += `
+        <div class="feedbacks__content-item">
+        <p class="feedbacks__content-item-person-info"> Новый пользователь <span> ${correctDate} </span></p>
+        <div class="feedbacks__content-item-comment">
+            <p> ${commentValue.value} </p>
+        </div>
+        </div>
+        `;
+    return (commentValue.value = "");
+  }
+};
+
+runOnKeys(sendCommentHendler, 13, 17);
+
+sendCommentBtn.addEventListener("click", function() {
+  sendCommentHendler();
+});
